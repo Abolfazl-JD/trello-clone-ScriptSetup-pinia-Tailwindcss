332910569a5b6ab5e6d415e1254460cb023c1575
abolfazl-hue <abolfazljdst@gmail.com>
2022-03-15 12:02:59 +0330

    hide formControl input when click outside

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,17 @@ const changeNewList = (val: boolean) => {
   setTimeout(() => {
     val ? formInp.value?.focus() : null
     showlistControl.value = val
-  }, 50);
-  
+  }, 50); 
+}
+
+const boardClick = () => {
+  console.log('parent clicked')
 }
 
 </script>
 
 <template>
-  <div class="board h-screen overflow-auto px-3 py-3 scrollbar scrollbar-thumb-gray-600 scrollbar-thin scrollbar-track-gray-900  ">
+  <div class="board h-screen overflow-auto px-3 py-3 scrollbar scrollbar-thumb-gray-600 scrollbar-thin scrollbar-track-gray-900" @click.self="changeNewList(false)">
     <div class="flex justify-start items-start gap-5">
       <div class="min-w-[280px] bg-gray-300 p-2 rounded-lg cursor-pointer">
         <div class="flex justify-between mb-2 px-2">
@@ -42,7 +45,7 @@ const changeNewList = (val: boolean) => {
         <p class="w-full h-full" @click="changeNewList(true)" v-if="!addNewList">+ Add another list</p>
         <template v-else>
           <input type="text" ref="formInp" placeholder="Enter list title..." class="px-3 py-2 text-sm w-full focus:outline-blue-600">
-          <div class="flex w-full mt-2 place-items-center space-x-2 transition-all ease-out duration-50" :class="!showlistControl ? 'h-0 overflow-hidden' : 'h-8'">
+          <div class="flex w-full mt-2 place-items-center space-x-2 transition-all ease-out duration-50" :class="!showlistControl ? 'h-0 overflow-hidden' : 'h-8'"  >
             <button class="bg-blue-600 py-2 px-[10px] rounded-sm text-sm text-white hover:bg-blue-700 transition-colors ease-out duration-300">Add list</button>
             <svg xmlns="http://www.w3.org/2000/svg" @click="changeNewList(false)" class="h-6 w-6 text-gray-700 transition-colors ease-out duration-200 hover:text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
