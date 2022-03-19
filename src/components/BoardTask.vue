7b7f703b7575a886f897b1a9d03a42f177f6eb6f
abolfazl-hue <abolfazljdst@gmail.com>
2022-03-19 18:28:07 +0330

    clean codes of board task

--- src/components/BoardTask.vue
+++ src/components/BoardTask.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type {Task} from '../types'
-import {boardColumns} from '../stores/board'
 import EditModal from './Modal.vue'
+
+import {boardColumns} from '../stores/board'
+const trelloBoard = boardColumns()
 
 interface propsType{
     task : Task,
@@ -14,7 +16,6 @@ const props = defineProps<propsType>()
 
 const showModal = ref(false)
 
-const trelloBoard = boardColumns()
 const toggleCheckTask = () => {
     trelloBoard.toggleCheckTask(props.task)
 }
@@ -25,10 +26,21 @@ const toggleCheckTask = () => {
     <div class="bg-white px-2 pb-[5px] pt-2 rounded mt-2 shadow shadow-gray-400">
         <div class="flex justify-between group"  @click="showModal = true">
             <p class="text-sm">{{ task.name }}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400 hidden group-hover:block" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+            <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                class="h-4 w-4 text-gray-400 hidden group-hover:block" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                stroke="currentColor" 
+                stroke-width="2">
+                <path 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
             </svg>
         </div>
+
+        <!-- task details (description, date, time, completed) -->
         <div class="flex gap-1 place-items-center">
             <div 
                 v-show="task.date" 
@@ -49,12 +61,23 @@ const toggleCheckTask = () => {
                 </svg>
                 <div class="hidden group-hover:block">
                     <div v-if="!task.completed" class="h-3 w-3 bg-transparent border-2 border-solid border-gray-400 rounded-sm"></div>
-                    <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-auto w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    <svg 
+                        v-else 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        class="h-auto w-4" 
+                        fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke="currentColor" 
+                        stroke-width="2">
+                        <path 
+                            stroke-linecap="round" 
+                            stroke-linejoin="round" 
+                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                 </div>
                 <p>{{task.date}} * {{task.time}}</p>
             </div>
+            <!-- task description icon -->
             <svg 
                 v-if="task.description"
                 @click="showModal = true"
